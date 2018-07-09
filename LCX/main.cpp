ddc27c262bf51479cf43e87b5dfd6b032ab9f27a
李晨曦 <lichenxi@lichenxidemacbook-pro.local>
2018-07-09 22:53:00 +0800

    Initial Commit

---
+++ LCX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LCX
+//
+//  Created by 李晨曦 on 2018/7/9.
+//  Copyright © 2018年 李晨曦. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
